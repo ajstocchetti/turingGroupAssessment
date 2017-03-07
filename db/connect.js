fa6a1061532a7471da9e20bb4751e67d5255e179
tackypinkflamingos <ajstocchetti@gmail.com>
2017-03-07 00:27:12 -0500

    make mongodb connector

---
+++ db/connect.js
@@ -0,0 +1,25 @@
+'use strict';
+const MongoClient = require('mongodb').MongoClient;
+let _db;
+
+function startDb() {
+    const uri = 'mongodb://localhost:27017/tgweather';
+    return MongoClient.connect(uri)
+    .then(db => {
+        console.log('Connected to mongodb');
+        _db = db;
+        return db;
+    }).catch(err => {
+        console.log(`Error connecting to mongodb: ${err}`);
+        throw err; // throw err after logging
+    });
+};
+
+function getDb() {
+    return _db;
+}
+
+module.exports = {
+    startDb,
+    getDb,
+};
---
+++ start.js
@@ -0,0 +1,9 @@
+'use strict';
+const db = require('./db/connect.js');
+
+db.startDb().then(() => {
+    require('./server.js');
+}).catch(err => {
+    console.log('Could not start server');
+    process.exit(1);
+});
